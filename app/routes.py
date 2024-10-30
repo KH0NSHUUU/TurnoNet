c73e326c0cdd8cc1d8f006b41e6851b4f5bfea40
KH0NSHUUU <khnshuuu9@gmail.com>
2024-10-30 16:35:06 -0300

    se realizo el apartado del administrador y del empleado

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
-from flask import render_template, request, redirect, session, url_for, flash
+from flask import render_template, request, redirect, session, url_for, flash, jsonify
 from app import app, mysql
-
 
 @app.route('/')
 def index():
@@ -22,7 +21,7 @@ def login():
             session['rol'] = user[6]  # Rol del usuario
 
             if user[6] == 'admin':
-                return redirect(url_for('admin'))
+                return redirect(url_for('admin_dashboard'))
             elif user[6] == 'empleado':
                 return redirect(url_for('empleado'))
             elif user[6] == 'cliente':
@@ -41,16 +40,9 @@ def register():
         contacto = request.form['contacto']
         correo = request.form['correo']
         contraseña = request.form['contraseña']
-
         cur = mysql.connection.cursor()
-        cur.execute(
-            'INSERT INTO usuarios (dni,nombre,apellido,contacto,correo,contraseña,rol) values (%s, %s, %s, %s, %s, %s, %s)',
-            (dni, nombre, apellido, contacto, correo, contraseña, 'cliente')
-        )
-        cur.execute(
-            'INSERT INTO clientes (dni,nombre,apellido,contacto,correo,contraseña,rol) values (%s, %s, %s, %s, %s, %s, %s)',
-            (dni, nombre, apellido, contacto, correo, contraseña, 'cliente')
-        )
+        cur.execute('INSERT INTO usuarios (dni,nombre,apellido,contacto,correo,contraseña,rol) values (%s, %s, %s, %s, %s, %s, %s)',(dni, nombre, apellido, contacto, correo, contraseña, 'cliente'))
+        cur.execute('INSERT INTO clientes (dni,nombre,apellido,contacto,correo,contraseña,rol) values (%s, %s, %s, %s, %s, %s, %s)',(dni, nombre, apellido, contacto, correo, contraseña, 'cliente'))
         mysql.connection.commit()
         cur.close()
 
@@ -58,14 +50,151 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
+
+# Todas las rutas del admin
+@app.route('/admin') #acceder a la pag del admin
+def admin_dashboard():
+    if 'rol' in session and session['rol'] == 'admin':
+        return render_template('admin.html')
+    else:
+        flash('Acceso denegado.', 'danger')
+        return redirect(url_for('login'))
+
+@app.route('/admin/servicios', methods=['GET','POST']) #muestra los servicios y agrega nuevos servicios desde el administrador
+def gestionar_servicios():#si es get lista los servicios
+    if request.method=='GET':
+        cur=mysql.connection.cursor()
+        cur.execute('SELECT * FROM servicios')
+        servicios=cur.fetchall()
+        cur.close()
+    
+        return jsonify([{'id':servicio[0], 'nombre':servicio[1], 'descripcion':servicio[2], 'precio':servicio[3]} for servicio in servicios])
+
+    if request.method=='POST':#si es post agrega uno nuevo
+        nombre=request.form['nombre']
+        descripcion=request.form['descripcion']
+        precio=request.form['precio']
+        cur=mysql.connection.cursor()
+        cur.execute('SELECT MAX(id) FROM servicios')
+        id=cur.fetchone()
+        idd=id[0]+1
+        cur.execute('INSERT INTO servicios (id,nombre,descripcion,precio) values (%s,%s,%s,%s)',(idd,nombre,descripcion,precio))
+        mysql.connection.commit()
+        cur.close()
+
+        return jsonify({'status':'success'})
+
+@app.route('/admin/servicios/<int:servicio_id>', methods=['PUT'])
+def modificar_servicio(servicio_id):#modifica los datos del empleado si es put
+    datos=request.get_json() 
+    cur=mysql.connection.cursor()
+
+    if 'nombre' in datos:
+        cur.execute('UPDATE servicios SET nombre =%s WHERE id=%s',(datos['nombre'],servicio_id))
+    if 'descripcion' in datos:
+        cur.execute('UPDATE servicios SET descripcion=%s WHERE id=%s',(datos['descripcion'],servicio_id))
+    if 'precio' in datos:
+        cur.execute('UPDATE servicios SET precio=%s WHERE id=%s',(datos['precio'],servicio_id))
+
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify({'status': 'success'})
+
+@app.route('/admin/servicios/<int:servicio_id>', methods=['DELETE'])
+def eliminar_servicio(servicio_id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM turnos WHERE id_servicio = %s', (servicio_id,))
+    cur.execute('DELETE FROM servicios WHERE id = %s', (servicio_id,))
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify({'status': 'success'})
+
+@app.route('/admin/empleados/<int:dni>', methods=['PUT'])
+def modificar_emplezados(dni):
+    datos=request.get_json()
+    print(datos)
+    cur=mysql.connection.cursor()
+
+    if 'dni' in datos:
+        cur.execute('UPDATE empleados SET dni=%s where dni=%s',(datos['dni'],dni))
+    if 'nombre' in datos:
+        cur.execute('UPDATE empleados SET nombre=%s where dni=%s',(datos['nombre'],dni))   
+    if 'apellido' in datos:
+        cur.execute('UPDATE empleados SET apellido=%s where dni=%s',(datos['apellido'],dni))
+    if 'contacto' in datos:
+        cur.execute('UPDATE empleados SET contacto=%s where dni=%s',(datos['contacto'],dni))
+    if 'correo' in datos:
+        cur.execute('UPDATE empleados SET correo=%s where dni=%s',(datos['correo'],dni))
+    if 'contraseña' in datos:
+        cur.execute('UPDATE empleados SET contraseña=%s where dni=%s',(datos['contraseña'],dni))
+
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify({'status': 'success'})
+
+@app.route('/admin/empleados/<int:dni>', methods=['DELETE'])
+def eliminar_empleados(dni):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM empleados WHERE dni = %s', (dni,))
+    cur.execute('DELETE FROM usuarios WHERE dni = %s', (dni,))
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify({'status': 'success'})
+
+@app.route('/admin/empleados', methods=['GET','POST'])
+def gestionar_empleados():
+    if request.method=='GET':
+        cur=mysql.connection.cursor()
+        cur.execute('SELECT * FROM empleados')
+        empleados=cur.fetchall()
+        cur.close()
+
+        return jsonify([{'dni':empleado[0],'nombre':empleado[1],'apellido':empleado[2],'contacto':empleado[3],'correo':empleado[4],'contraseña':empleado[5]} for empleado in empleados])
+    
+    if request.method=='POST':
+        dni=request.form['dni']
+        nombre=request.form['nombre']
+        apellido=request.form['apellido']
+        contacto=request.form['contacto']
+        correo=request.form['correo']
+        contraseña=request.form['contraseña']
+        cur=mysql.connection.cursor()
+        cur.execute('INSERT INTO usuarios (dni,nombre,apellido,contacto,correo,contraseña,rol) values (%s,%s,%s,%s,%s,%s,%s)',(dni,nombre,apellido,contacto,correo,contraseña,'empleado'))
+        cur.execute('INSERT INTO empleados (dni,nombre,apellido,contacto,correo,contraseña,rol) values (%s,%s,%s,%s,%s,%s,%s)',(dni,nombre,apellido,contacto,correo,contraseña,'empleado'))
+        mysql.connection.commit()
+        cur.close()
+
+        return jsonify({'status':'success'})
+
+
+# Empleados
 @app.route('/empleado')
 def empleado():
-    return render_template('empleado.html')
+    if 'rol' in session and session['rol'] == 'empleado':
+        return render_template('empleado.html')
+    else:
+        flash('Acceso denegado.', 'danger')
+        return redirect(url_for('login'))
+    
+@app.route('/empleado/perfil', methods=['PUT'])
+def modificar_perfil_empleado():
+    pass
+    
 
+# Clientes
 @app.route('/cliente')
 def cliente():
-    return render_template('cliente.html')
+    if 'rol' in session and session['rol'] == 'cliente':
+        return render_template('cliente.html')
+    else:
+        flash('Acceso denegado.', 'danger')
+        return redirect(url_for('login'))
